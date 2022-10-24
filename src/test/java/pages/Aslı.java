5dc5405bd6adf727c342e68d811465a40ea69843
Aslı Över <aslover1991@gmail.com>
2022-10-24 14:04:43 +0300

    asli1

--- src/test/java/pages/Aslı.java
+++ src/test/java/pages/Aslı.java
@@ -1,4 +1,32 @@
 package pages;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import utilities.Driver;
+
 public class Aslı {
+    public Aslı() {
+        PageFactory.initElements(Driver.getDriver(), this);
+    }
+
+    @FindBy(xpath = "//*[text()='Sign In']")
+    public WebElement signInButonu;
+    @FindBy(xpath = "//*[@id='username']")
+    public WebElement userEmail;
+    @FindBy(xpath = "(//*[text()='My Account'])[2]")
+    public WebElement myAccountButonu;
+    @FindBy(xpath = "//*[text()='Store Manager']")
+    public WebElement storeManagerButonu;
+    @FindBy(xpath = "(//*[@class='text'])[6]")
+    public WebElement couponsButonnu;
+    @FindBy(xpath = "(//*[text()='Add New'])[1]")
+    public WebElement addNewButonu;
+    @FindBy(xpath = "//*[@id='title']")
+    public WebElement codeTextBox;
+    @FindBy(xpath = "(//*[@class='wcfm_submit_button'])[1]")
+    public WebElement submitButonu;
+    @FindBy(xpath = "//*[contains(text(),'Coupon Successfully Published')]")
+    public WebElement basariliGiris;
+
 }
--- src/test/java/team07/US0013.java
+++ src/test/java/team07/US0013.java
@@ -1,4 +1,39 @@
 package team07;
 
+import org.openqa.selenium.Keys;
+import org.testng.annotations.Test;
+import pages.Aslı;
+import utilities.ConfigReader;
+import utilities.Driver;
+import utilities.ReusableMethods;
+
 public class US0013 {
+    Aslı aslı;
+    @Test
+    public void TC_001() {
+
+       //Kullanici url` ye gider
+        Driver.getDriver().get(ConfigReader.getProperty("url"));
+       //Kullanici Sign In tiklar
+        aslı=new Aslı();
+        aslı.signInButonu.click();
+       //Kullanici gecerli e-mail 'i girer
+        aslı.userEmail.sendKeys(ConfigReader.getProperty("email"));
+       //Kullanici gecerli password girer
+        ReusableMethods.waitFor(3);
+        ReusableMethods.getActions().sendKeys(Keys.TAB).sendKeys(ConfigReader.getProperty("password")).perform();
+       //Kullanici SIGN IN butonuna tiklar
+        ReusableMethods.waitFor(3);
+        ReusableMethods.getActions().sendKeys(Keys.TAB).sendKeys(Keys.TAB).sendKeys(Keys.TAB).sendKeys(Keys.ENTER).perform();
+       //Kullanici My Account butonuna tiklar
+        ReusableMethods.waitFor(2);
+        ReusableMethods.
+       //Kullanici Store Manager butonuna tiklar
+       //Kullanici Coupons butonuna tiklar
+       //Kullanici Add New butonuna tiklar
+       //Kullanici code metin kutusuna rastgele 8 basamakli sayi girer
+       //Kullanici submit butonuna tiklar
+       //Coupon kodu verildigi dogrulanir
+       //Sayfa kapatilir
+    }
 }
