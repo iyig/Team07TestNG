785a685b6d54effed81c5566250a774c41631e0c
Mett <metinkvss@gmail.com>
2022-10-24 16:50:38 +0300

    US003 US004 Added

--- src/test/java/team07/US003.java
+++ src/test/java/team07/US003.java
@@ -1,4 +1,99 @@
-package team07;
+package tests;
+
+import com.github.javafaker.Faker;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import pages.Metin;
+import utilities.ConfigReader;
+import utilities.Driver;
+import utilities.ReusableMethods;
 
 public class US003 {
+    @Test
+    public void US003() throws InterruptedException {
+        Metin pearlyPage = new Metin();
+
+        // 1- https://pearlymarket.com/ sitesine gidiniz
+        Driver.getDriver().get(ConfigReader.getProperty("url"));
+
+        // 2- Giris Yap/Uye Ol butonuna basiniz
+        pearlyPage.girisYap.click();
+
+        // 3- Uygun email ve sifre giriniz.
+        pearlyPage.username.sendKeys(ConfigReader.getProperty("email"));
+        pearlyPage.password.sendKeys(ConfigReader.getProperty("sifre"));
+        pearlyPage.girisYap2.click();
+
+        // 4- myorders butonuna tiklayin
+        ReusableMethods.waitFor(2);
+        JavascriptExecutor js = (JavascriptExecutor) Driver.getDriver();
+        js.executeScript("arguments[0].click();", pearlyPage.myOrders);
+        //pearlyPage.myOrders.click();
+
+        // 5- browse products (view)
+        js.executeScript("arguments[0].click();", pearlyPage.browseProducts);
+        //pearlyPage.browseProducts.click();
+
+        // 6- sepete 5 urun ekle
+
+        /*WebDriver driver=new ChromeDriver();
+        driver.get(driver.getCurrentUrl());
+        Actions actions = new Actions(driver);
+        actions.moveToElement(driver.findElement(By.xpath("//div[@class='product-loop product-default content-center product type-product post-13861 status-publish first instock product_cat-best-seller product_cat-electronics product_cat-trending has-post-thumbnail sale virtual purchasable product-type-simple']//img[2]"))).perform();
+        driver.findElement(By.xpath("//div[@class='product-loop product-default content-center product type-product post-13861 status-publish first instock product_cat-best-seller product_cat-electronics product_cat-trending has-post-thumbnail sale virtual purchasable product-type-simple']//a[@aria-label='Add “USB Receipt” to your cart'][normalize-space()='Add to cart']")).click();
+        */
+        js.executeScript("arguments[0].click();", pearlyPage.birinciUrun);
+        js.executeScript("arguments[0].click();", pearlyPage.ikinciUrun);
+        js.executeScript("arguments[0].click();", pearlyPage.ucuncuUrun);
+        js.executeScript("arguments[0].click();", pearlyPage.dorduncuUrun);
+        js.executeScript("arguments[0].click();", pearlyPage.besinciUrun);
+        //  pearlyPage.birinciUrun.click();
+        //  pearlyPage.ikinciUrun.click();
+        //  pearlyPage.ucuncuUrun.click();
+        //  pearlyPage.dorduncuUrun.click();
+        //  pearlyPage.besinciUrun.click();
+
+        // 7- sepetim ve odeme butonuna bas
+        pearlyPage.sepetim.click();
+        pearlyPage.checkout.click();
+
+        // 8- Fatura detaylarını eksiksiz olarak gir
+
+        Actions actions = new Actions(Driver.getDriver());
+        Faker faker = new Faker();
+
+        actions.click(pearlyPage.faturaBilgileri)
+                .sendKeys(faker.name().firstName())
+                .sendKeys(Keys.TAB)
+                .sendKeys(faker.name().lastName())
+                .sendKeys(Keys.TAB)
+                .sendKeys(Keys.TAB)
+                .sendKeys("1234")
+                .sendKeys(Keys.TAB)
+                .sendKeys("571")
+                .sendKeys(Keys.TAB)
+                .sendKeys("1234")
+                .sendKeys(Keys.TAB)
+                .sendKeys(faker.address().cityName())
+                .sendKeys(Keys.TAB)
+                .sendKeys(Keys.ARROW_DOWN)
+                .sendKeys(Keys.ARROW_DOWN)
+                .sendKeys(Keys.ARROW_DOWN)
+                .sendKeys(Keys.ARROW_DOWN)
+                .sendKeys(Keys.ENTER)
+                .sendKeys(Keys.TAB)
+                .sendKeys(faker.phoneNumber().phoneNumber())
+                .sendKeys(Keys.TAB)
+                .sendKeys(faker.internet().emailAddress())
+                .perform();
+        ReusableMethods.waitFor(5);
+        // 9- siparisi onayla butonuna bas ve siparisin onaylandigini dogrula
+        js.executeScript("arguments[0].click();", pearlyPage.placeOrder);
+        // pearlyPage.placeOrder.click();
+        Assert.assertTrue(pearlyPage.siparisAlindi.isDisplayed(), "Siparisiniz Alinmistir");
+        ReusableMethods.waitFor(5);
+    }
 }
--- src/test/java/team07/US004.java
+++ src/test/java/team07/US004.java
@@ -1,4 +1,149 @@
-package team07;
+package tests;
+
+import com.github.javafaker.Faker;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import pages.Metin;
+import utilities.ConfigReader;
+import utilities.Driver;
+import utilities.ReusableMethods;
+
+import java.io.IOException;
+import java.time.Duration;
+
 
 public class US004 {
+    Metin pearlyPage = new Metin();
+    Actions actions = new Actions(Driver.getDriver());
+
+    @Test
+    public void test01SepeteUrunEkle() throws InterruptedException, IOException {
+
+        // 1. SEÇİLEN ÜRÜNLER VİEW CARTTA GÖRÜLMELİ: RAKAM (fiyat), MİKTAR ve (ara) TOPLAM OLARAK GÖRÜLMELİ
+        Driver.getDriver().get(ConfigReader.getProperty("url"));
+        ReusableMethods.waitFor(5);
+        //pearlyPage.shopNow.click();
+        JavascriptExecutor js = (JavascriptExecutor) Driver.getDriver();
+        js.executeScript("arguments[0].click();", pearlyPage.urun);
+        // pearlyPage.urun.click(); //Bir ürüne tıklanır
+        pearlyPage.addChart.click(); //ve sepete eklenir.
+        ReusableMethods.waitFor(5);
+        pearlyPage.chart.click();
+        ReusableMethods.waitFor(5);
+        pearlyPage.viewChart.click(); //View charta tıklanır.
+        WebDriverWait wait = new WebDriverWait(Driver.getDriver(), Duration.ofSeconds(15));
+        Thread.sleep(3000);
+
+        Assert.assertTrue(pearlyPage.sepettekiUrunResmi.isDisplayed()); //Sepete eklenen ürünler görünüyor mu kontrol edilir.
+
+        Assert.assertTrue(pearlyPage.sepettekiFiyat.isEnabled()); //Ürün fiyatı görünüyor mu
+
+        Assert.assertTrue(pearlyPage.sepettekiUrunMiktari.isDisplayed()); //Ürün miktarı görünüyor mu
+
+        Assert.assertTrue(pearlyPage.sepettekiToplamFiyat.isEnabled());
+
+// 2. ARTI ile URUN RAKAMI ARTIRILABİLMELİ ve EKSİ ile RAKAMLAR DÜŞÜRÜLEBİLMELİ
+        ReusableMethods.waitFor(5);
+        pearlyPage.urunArttirmaTusu.click();
+        ReusableMethods.getScreenshot("urun arttı");
+        pearlyPage.urunEksiltmeTusu.click();
+        ReusableMethods.getScreenshot("urun eksildi");
+
+
+       /* String sepettekiIlkUrunMiktari = pearlyPage.sepettekiUrunMiktariKutusu.getAttribute("value");
+        int value = Integer.parseInt(sepettekiIlkUrunMiktari);
+
+        System.out.println("Urun İlk Miktarı = " + sepettekiIlkUrunMiktari);
+        pearlyPage.urunArttirmaTusu.click();
+
+        Thread.sleep(3000); // update için gerekli zaman
+        String sepettekiIkinciUrunMiktari = pearlyPage.sepettekiUrunMiktariKutusu.getAttribute("value");
+        Assert.assertTrue(Integer.parseInt(sepettekiIkinciUrunMiktari) > Integer.parseInt(sepettekiIlkUrunMiktari));
+        ReusableMethods.getScreenshot("Artı butonu ile miktar bir arttı");
+        pearlyPage.urunEksiltmeTusu.click();
+        String sepettekiUcuncuUrunMiktari = pearlyPage.sepettekiUrunMiktariKutusu.getAttribute("value");
+        Assert.assertTrue(Integer.parseInt(sepettekiUcuncuUrunMiktari) < Integer.parseInt(sepettekiIkinciUrunMiktari));
+        ReusableMethods.getScreenshot("Eksi butonu ile miktar bir azaldı"); */
+
+// 3 STOK FAZLASI URUN EKLENEMEMELİ ve UPDATE CART ile RAKAM ORTALAMALARI GÜNCELLENMELİ
+// 3.a Manuel giriş ile stok fazlası girilememeli :
+        Driver.getDriver().get(ConfigReader.getProperty("asusUrl"));
+        Assert.assertTrue(pearlyPage.asusStokMiktarı.isDisplayed());
+        ReusableMethods.waitFor(5);
+
+        pearlyPage.addChart.click();
+        pearlyPage.urunArttirmaTusu.click();
+        pearlyPage.addChart.click();
+
+        //pearlyPage.asusQuantityBox.click();
+        actions.sendKeys("4").click(pearlyPage.addChart).perform();
+        Assert.assertTrue(pearlyPage.addChart.isDisplayed());
+        ReusableMethods.getScreenshot("Sepete stok miktarından fazla ürün eklenemedi");
+        ReusableMethods.waitFor(7);
+
+// 4. CLEAR CART ile RAKAMLAR SİLİNEBİLMELİ:
+        js.executeScript("arguments[0].click();", pearlyPage.chart2);
+
+        pearlyPage.viewChart.click();
+        pearlyPage.clearChart.click();
+        Assert.assertTrue(pearlyPage.sepetBosYazisi.isDisplayed());
+        ReusableMethods.getScreenshot("Your cart is currently empty.");
+
+// 5. COUPON SECENEĞİ OLMALI
+
+        pearlyPage.magazayaDon.click();
+        ReusableMethods.waitFor(5);
+        pearlyPage.urunLaptop.click();
+        ReusableMethods.waitFor(5);
+        pearlyPage.addChart.click();
+        pearlyPage.chart.click();
+        pearlyPage.viewChart.click();
+        Assert.assertTrue(pearlyPage.applyCoupon.isDisplayed());
+
+// 6. TEKRAR SHOPPİNG DEVAM ETME BUTONU OLMALI
+        Assert.assertTrue(pearlyPage.alışveriseDevamEt.isDisplayed());
+
+//7. SHİPİNG BİLGİLERİ SAG TARAFTA GÖRÜLMELİ ve TOPLAM RAKAM SAĞDA POPULATE ETMELİ
+
+        Assert.assertTrue(pearlyPage.chartTotals.isDisplayed());
+
+        pearlyPage.satınAl.click();
+// 8 PROCEED TO CHECKOUT BASARILI BİR ŞEKİLDE ÖDENEKLERE YÖNLENDİRİLMELİ:
+        Actions actions = new Actions(Driver.getDriver());
+        Faker faker = new Faker();
+        ReusableMethods.waitFor(5);
+        actions.click(pearlyPage.faturaBilgileri)
+                .sendKeys(faker.name().firstName())
+                .sendKeys(Keys.TAB)
+                .sendKeys(faker.name().lastName())
+                .sendKeys(Keys.TAB)
+                .sendKeys(Keys.TAB)
+                .sendKeys("1234")
+                .sendKeys(Keys.TAB)
+                .sendKeys("571")
+                .sendKeys(Keys.TAB)
+                .sendKeys("1234")
+                .sendKeys(Keys.TAB)
+                .sendKeys(faker.address().cityName())
+                .sendKeys(Keys.TAB)
+                .sendKeys(Keys.ARROW_DOWN)
+                .sendKeys(Keys.ARROW_DOWN)
+                .sendKeys(Keys.ARROW_DOWN)
+                .sendKeys(Keys.ARROW_DOWN)
+                .sendKeys(Keys.ENTER)
+                .sendKeys(Keys.TAB)
+                .sendKeys(faker.phoneNumber().phoneNumber())
+                .sendKeys(Keys.TAB)
+                .sendKeys(faker.internet().emailAddress())
+                .perform();
+        ReusableMethods.waitFor(5);
+        //Proceed to checkout ta tıklanır ve ödeme ekranına ulaşıp ulaşmadığı kontrol edilir.
+        pearlyPage.placeOrder.click();
+        Assert.assertTrue(pearlyPage.siparisAlindi.isDisplayed());
+        Driver.closeDriver();
+    }
 }
